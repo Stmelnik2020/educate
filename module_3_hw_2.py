cf457d17d32aa787a33f7c15d687cda1672e704d
stmelnik2020 <stmelnik2020@gmail.com>
2025-08-27 15:13:54 +0200

    completed homework module 3 task 2 (code formatting update)

--- module_3_hw_2.py
+++ module_3_hw_2.py
@@ -7,9 +7,9 @@ def get_days_from_today(date: str) -> int:
     '''
     a function that calculates the number of days between a given date and the current date.
     '''
-    days_left = (datetime.strptime(date, "%Y.%m.%d").date(
-        # subtract the current date from the specified date
-    ) - datetime.today().date()).days
+    # subtract the current date from the specified date
+    days_left = (datetime.strptime(date, "%Y.%m.%d").date() -
+                 datetime.today().date()).days
     return days_left
 
 
