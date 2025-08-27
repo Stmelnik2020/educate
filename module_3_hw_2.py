a29bf1ffdfc11c34589ab3b58b92a3969c41699f
stmelnik2020 <stmelnik2020@gmail.com>
2025-08-27 15:18:48 +0200

    add try except block

--- module_3_hw_2.py
+++ module_3_hw_2.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date
 
-set_date = '2025.10.09'
+set_date = '202s5.10.09'
 
 
 def get_days_from_today(date: str) -> int:
@@ -8,9 +8,11 @@ def get_days_from_today(date: str) -> int:
     a function that calculates the number of days between a given date and the current date.
     '''
     # subtract the current date from the specified date
-    days_left = (datetime.strptime(date, "%Y.%m.%d").date() -
-                 datetime.today().date()).days
-    return days_left
-
+    try:
+        days_left = (datetime.strptime(date, "%Y.%m.%d").date() -
+                    datetime.today().date()).days
+        return days_left
+    except ValueError:
+        return f'{set_date} is not date! Write date in format "YYYY.MM.DD'
 
 print(get_days_from_today(set_date))
